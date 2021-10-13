e3bf5f8bf1b65eed038b68ef1b2a472c5d729ddf
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-13 20:59:16 +0000

    breyta e-ð

--- bin/question1.dart
+++ bin/question1.dart
@@ -1,3 +1,8 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  List<int> calculate_sum = [13,56,77,3,45,6,944,1,43,7,40,55,67,93,45,754,33];
+  int sum = 0;
+  for (int i in calculate_sum){
+    sum += i;
+  }
+  print(sum);
 }
