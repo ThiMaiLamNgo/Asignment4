1ab62800c7b85dbaaeab74f207a8576af8db3273
ThiMaiLamNgo <thimailamngo@gmail.com>
2021-10-17 11:04:45 +0000

    Assignment4-q4: OK

--- bin/question4_4.dart
+++ bin/question4_4.dart
@@ -17,18 +17,39 @@ bool  getYesNo(String prompt){
     return ans;
   }
 }
+
 List<String> getStringList(String prompt){
   print(prompt);
   List<String> myString = [];
   while(!getYesNo('Do you want to stop? Enter "no" to continue OR press enter to stop')) {
-    myString.add(getString('Enter next characters'));
+    myString.add(getString('Enter next string'));
   }
   return myString;
+}
 
-}
 void main() {
-  List<String> listString = getStringList('Give me some characters! ');
+  List<String> listString = getStringList('Give me a string! ');
   List<String> listReversed =  (listString.reversed).toList();
-  print(listString);
-  print(listReversed);
+  print("The original list: $listString");
+  print('The reversed list: $listReversed');
 }
+
+/*
+Give me a string!
+Do you want to stop? Enter "no" to continue OR press enter to stop
+no
+Enter next string
+uno
+Do you want to stop? Enter "no" to continue OR press enter to stop
+no
+Enter next string
+win
+Do you want to stop? Enter "no" to continue OR press enter to stop
+no
+Enter next string
+I
+Do you want to stop? Enter "no" to continue OR press enter to stop
+
+The original list: [uno, win, I]
+The reversed list: [I, win, uno]
+ */
